5fb1f8d557e77bc211189b84e1b5a527438eeac9
Gabriel <tuxomaiworm@hotmail.com>
2022-04-15 11:31:16 -0300

    update

---
+++ src/main/java/com/biomob/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.biomob.service.dto;
---
+++ src/test/java/com/biomob/cucumber/CucumberIT.java
@@ -0,0 +1,8 @@
+package com.biomob.cucumber;
+
+import com.biomob.IntegrationTest;
+import io.cucumber.junit.platform.engine.Cucumber;
+
+@Cucumber
+@IntegrationTest
+class CucumberIT {}
